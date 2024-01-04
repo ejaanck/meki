8c6522982198b490eb2bbf043aa21ffa48b1b93f
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-01-04 22:35:34 +0700

    Update __init__.py

--- Ayra/startup/__init__.py
+++ Ayra/startup/__init__.py
@@ -49,7 +49,7 @@ if run_as_module:
         os.remove(file)
 
     HOSTED_ON = where_hosted()
-    LOGS = getLogger("Key-Userbot")
+    LOGS = getLogger("Fake-Ubot")
     TelethonLogger = getLogger("Telethon")
     TelethonLogger.setLevel(INFO)
 
@@ -89,9 +89,9 @@ if run_as_module:
     )
 
     LOGS.info(f"Python version - {platform.python_version()}")
-    LOGS.info(f"Key Version - {__Ayra__}")
+    LOGS.info(f"Fake Version - {__Ayra__}")
     LOGS.info(f"Telethon Version - {__version__} [Layer: {LAYER}]")
-    LOGS.info(f"Key-Userbot Version - {ayra_version} [{HOSTED_ON}]")
+    LOGS.info(f"Fake-Ubot Version - {ayra_version} [{HOSTED_ON}]")
 
     try:
         from safety.tools import *
